1cc168319f025a1352f6b87a990776acd9a73af1
Echika <85797519+Echika@users.noreply.github.com>
2022-04-25 14:25:20 +0100

    First commit

---
+++ color_flipper.js
@@ -0,0 +1,19 @@
+const colors = ['red', 'orange', 'rebeccapurple', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const btn = document.getElementById('btn');
+
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', () => {
+
+    const randomNumber = getRandomNumber();
+
+    document.body.style.backgroundColor = colors[randomNumber];
+
+    color.textContent = colors[randomNumber];
+
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length);
+}
